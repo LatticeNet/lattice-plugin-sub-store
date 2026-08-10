4567e50f3d9d3d36c5897f3f051ea2dd5feb6e23
lr00rl <lidongguinb@gmail.com>
2026-08-10 07:33:56 -0700

    chore: sign 0.12.0-alpha.3

--- system-go/main.go
+++ system-go/main.go
@@ -32,7 +32,7 @@ import (
 const (
 	pluginID             = "latticenet.sub-store"
 	pluginName           = "Sub-Store companion"
-	pluginVersion        = "0.12.0-alpha.2"
+	pluginVersion        = "0.12.0-alpha.3"
 	pipelineRecordsKey   = "engine-pipelines-v1"
 	maxExportLinks       = 10_000
 	maxExportBytes       = 1 << 20
